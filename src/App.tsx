6993d45a77bf0f2ab48aac71841accccb41e33d0
Rayched <ny2401@naver.com>
2025-01-27 15:29:21 +0900

    Project Reform

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,7 @@
-//Daily Planner 초안
-/**
- * 아주 기본적인 형태의 To Do List
- * 금일 일정, 시작 시간, 종료시간을 입력하고
- * '일정 추가' 버튼을 클릭하면
- * 일정 입력 form 하단에 지금까지 추가했던 일정들이 나온다.
- */
-
-import DailyPlanner from "./Components/DailyPlanner";
 
 function App(){
   return (
     <div>
-      <DailyPlanner />
     </div>
   );
 }
