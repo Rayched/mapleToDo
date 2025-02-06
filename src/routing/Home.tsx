fd8df3bbc09f5f4c1776cdde54622e511fe82539
Rayched <ny2401@naver.com>
2025-02-06 23:05:07 +0900

    Home 화면 임시 Style setting

--- src/routing/Home.tsx
+++ src/routing/Home.tsx
@@ -16,22 +16,98 @@ const Homes = styled.div`
     justify-content: center;
 `;
 
-const Titles = styled.header`
+const Headers = styled.header`
     display: flex;
     justify-content: center;
     align-items: center;
     padding: 5px 0px;
+    margin: 10px 0px;
+`;
 
-    .icons {
-        width: 25px;
-        height: 25px;
-        background: inherit;
-    }
+const Titles = styled.div`
+    font-size: 2em;
+    font-weight: bold;
+    padding: 0px 5px;
+`;
 
-    h3 {
-        font-size: 20px;
-        font-weight: bold;
+const Icons = styled.img`
+    width: 2em;
+    height: 2em;
+    background: inherit;
+`;
+
+const Mains = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 10px;
+`;
+
+const CharacterForm = styled.form`
+    padding: 5px;
+    border: 2px solid black;
+    border-radius: 18px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: rgb(225, 218, 218);
+`;
+
+const SearchBar = styled.input`
+    width: 30em;
+    height: 2em;
+    border: 0px;
+    padding-left: 5px;
+    background-color: inherit;
+
+    &:focus {
+        outline: none;
     }
+`;
+
+const SearchBtn = styled.button`
+    width: 2.4em;
+    height: 2em;
+    background-color: inherit;
+    border: 0px;
+`;
+
+const Bookmark_Box = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin-top: 15px;
+    padding: 10px;
+    border-radius: 10px;
+    background-color: rgb(225, 218, 218);
+`;
+
+const B_Title = styled.div`
+    font-size: 16px;
+    font-weight: bold;
+`;
+
+const Bookmarks = styled.ul`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 10px 0px;
+`;
+
+const Bookmark_Item = styled.li`
+    width: 20em;
+    padding: 5px 0px;
+    margin: 2px 0px;
+    background-color: rgb(245, 240, 240);
+    border-radius: 10px;
+    text-align: center;
+    font-size: 14px;
+    font-weight: bold;
+    &:hover {
+        background-color: rgb(250, 250, 250);
+    };
 `;
 
 function Home(){
@@ -47,30 +123,62 @@ function Home(){
         Navigate(`/${data.charNm}`);
     };
 
+    const TestItems = [
+        {
+            charNm: "손곈",
+            level: 271,
+            world: "스카니아",
+            char_class: "루미너스"
+        },
+        {
+            charNm: "경력직용기사",
+            level: 261,
+            world: "스카니아",
+            char_class: "카이저"
+        },
+        {
+            charNm: "알리오Olio",
+            level: 261,
+            world: "챌린저스2",
+            char_class: "데몬 슬레이어"
+        }
+    ];
+
     return (
         <Homes>
-            <Titles>
-                <img className="icons" src="maple_icons.png"/>
-                <h3>
-                    Maple Scheduler in Web
-                </h3>
-            </Titles>
-            <form onSubmit={handleSubmit(onValid)}>
-                <input 
-                    type="text" 
-                    placeholder="캐릭터 이름을 입력해주세요."
-                    {...register("charNm", {required: "캐릭터 이름을 입력해주세요."})}
-                />
-                <button>검색</button>
-            </form>
-            <div>
-                <h4>북마크 목록</h4>
-                <div>
-                    <div>손곈</div>
-                    <div>경력직용기사</div>
-                    <div>알리오Olio</div>
-                </div>
-            </div>
+            <Headers>
+                <Icons src="maple_icons.png"/>
+                <Titles>메이플 스케줄러 in Web</Titles>
+            </Headers>
+            <Mains>
+                <CharacterForm onSubmit={handleSubmit(onValid)}>
+                    <SearchBar 
+                        type="text" 
+                        placeholder="캐릭터 이름을 입력해주세요."
+                        autoComplete="off"
+                        {...register("charNm", {required: "캐릭터 이름을 입력해주세요."})}
+                    />
+                    <SearchBtn>
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                            <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/>
+                        </svg>
+                    </SearchBtn>
+                </CharacterForm>
+            </Mains>
+            <Bookmark_Box>
+                <B_Title>북마크</B_Title>
+                <Bookmarks>
+                    {
+                        TestItems.map((char) => {
+                            return (
+                                <Bookmark_Item>
+                                    {char.charNm} / Lv {char.level} / {char.char_class}
+                                </Bookmark_Item>
+                            );
+                        })
+                    }
+                </Bookmarks>
+            </Bookmark_Box>
         </Homes>
     );
 };
