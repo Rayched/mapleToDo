993b075dfe8581e767e57d1a66d587d9fed3f3ed
Rayched <ny2401@naver.com>
2025-06-11 15:39:38 +0900

    삭제 버튼, 완료 버튼(checkbox) 디자인 변경

--- src/routing/DetailPage/ToDoItems/BasedToDo.tsx
+++ src/routing/DetailPage/ToDoItems/BasedToDo.tsx
@@ -30,7 +30,22 @@ const ToDoItem = styled.li<I_Checked>`
     background-color: ${(props) => props.isDone ? "rgb(99, 110, 114)" : "rgb(220, 221, 225)"};
 `;
 
-const CheckBox = styled.input``;
+const CheckBox = styled.div<I_Checked>`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 1em;
+    height: 1em;
+    background-color: ${(props) => props.isDone ? "rgb(220, 221, 225)" : "rgb(250, 250, 250)"};
+    border: 2px solid black;
+    border-radius: 20px;
+`;
+
+const CheckIcon = styled.span<I_Checked>`
+    display: ${(props) => props.isDone ? "flex" : "none"};
+    font-weight: bold;
+    font-size: 16px;
+`;
 
 const ContentsBody = styled.div<I_Checked>`
     width: 85%;
@@ -39,17 +54,24 @@ const ContentsBody = styled.div<I_Checked>`
     justify-content: center;
     align-items: center;
     text-align: center;
-    font-size: 15px;
+    font-size: 16px;
     font-weight: bold;
     color: ${(props) => props.isDone ? "lightgray" : "black"};
     text-decoration: ${(props) => props.isDone ? "line-through" : "none"};
 `;
 
-const DeleteBtn = styled.button`
+const DeleteBtn = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    width: 50px;
+    background-color: rgb(255, 121, 121);
+    border: 2px solid rgb(250, 250, 250);
+    border-radius: 20px;
+    padding: 1px;
+
+    &:hover {
+        background-color: rgb(252, 98, 98);
+    };
 `;
 
 function BasedToDo({ToDoId, isDones, children}: I_BasedToDo){
@@ -104,16 +126,20 @@ function BasedToDo({ToDoId, isDones, children}: I_BasedToDo){
 
     return (
         <ToDoItem isDone={isDones}>
-            <CheckBox
-                type="checkbox" 
-                checked={isDones}
-                onChange={() => onChange(ToDoId)}
-            />
+            <CheckBox isDone={isDones} onClick={() => onChange(ToDoId)}>
+                <CheckIcon isDone={isDones}>✔</CheckIcon>
+            </CheckBox>
             <ContentsBody isDone={isDones}>
                 {children}
             </ContentsBody>
             {
-                IsEdits ? <DeleteBtn onClick={() => onDelete(ToDoId)}>삭제</DeleteBtn> : null
+                IsEdits ? (
+                    <DeleteBtn onClick={() => onDelete(ToDoId)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="13" width="13" viewBox="0 0 384 512">
+                            <path fill="#fafafa" d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/>
+                        </svg>
+                    </DeleteBtn>
+                ) : null
             }
         </ToDoItem>
     );
