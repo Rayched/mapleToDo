b63654da41c7a9751563b5da136e2bac0958cfc6
Rayched <ny2401@naver.com>
2025-06-15 22:05:24 +0900

    북마크 삭제 기능 추가

--- src/routing/Home.tsx
+++ src/routing/Home.tsx
@@ -1,9 +1,10 @@
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import { useRecoilValue, useSetRecoilState } from "recoil";
+import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 import { A_CharNmSaves, OcidAtoms } from "../Atoms";
 import { API_Keys } from "../modules/datas/APIs";
+import { useState } from "react";
 
 interface I_Charactors {
     charNm: string;
@@ -15,6 +16,11 @@ interface I_Outputs {
         message?: string;
         name?: string;
     }
+};
+
+interface I_EditMode {
+    isEdits: boolean;
+    isBookmark?: boolean;
 };
 
 const Homes = styled.div`
@@ -87,7 +93,7 @@ const Bookmark_Box = styled.div`
     flex-direction: column;
     align-items: center;
     width: 20em;
-    height: 7em;
+    height: 10em;
     margin-top: 15px;
     padding: 10px;
     border-radius: 10px;
@@ -99,6 +105,28 @@ const B_Title = styled.div`
     font-weight: bold;
 `;
 
+const EditBtn = styled.div<I_EditMode>`
+    display: ${(props) => props.isBookmark ? "flex" : "none"};
+    justify-content: center;
+    align-items: center;
+    padding: 3px;
+    margin-top: 6px;
+
+    background-color: ${(props) => props.isEdits ? "rgb(108, 116, 126)" : "rgb(83, 92, 104)"};
+    color: rgb(250, 250, 250);
+
+    border: 2px solid black;
+    border-radius: 10px;
+
+    box-shadow: 0.2px 0.5px rgba(0, 0, 0, 0.8);
+
+    .EditBtnText {
+        padding: 0px 2px;
+        margin: 0px 2px;
+        font-weight: bold;
+    };
+`;
+
 const Bookmarks = styled.ul`
     display: flex;
     flex-direction: column;
@@ -107,8 +135,13 @@ const Bookmarks = styled.ul`
     padding: 10px 0px;
 `;
 
-const Bookmark_Item = styled.li`
+const Bookmark_Item = styled.li<I_EditMode>`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
     width: 20em;
+    height: 1.5em;
     padding: 5px 0px;
     margin: 2px 0px;
     background-color: rgb(245, 240, 240);
@@ -119,6 +152,25 @@ const Bookmark_Item = styled.li`
     &:hover {
         background-color: rgb(250, 250, 250);
     };
+
+    .BookmarkText {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: ${(props) => props.isEdits ? "90%" : "100%"};
+    }
+`;
+
+const DeleteBtn = styled.div<I_EditMode>`
+    display: ${(props) => props.isEdits ? "flex" : "none"};
+    justify-content: center;
+    align-items: center;
+    border: 2px solid white;
+    border-radius: 25px;
+    width: 1.3em;
+    height: 1.3em;
+    background-color: rgb(255, 121, 121);
+    margin: 0px 2px;
 `;
 
 /**
@@ -131,10 +183,12 @@ const Bookmark_Item = styled.li`
 function Home(){
     const Navigate = useNavigate();
 
-    const SaveNms = useRecoilValue(A_CharNmSaves);
+    const [SaveNms, setSaveNms] = useRecoilState(A_CharNmSaves);
     const setCharID = useSetRecoilState(OcidAtoms);
 
     const {register, handleSubmit, setValue} = useForm<I_Charactors>();
+
+    const [Edits, setEdits] = useState(false);
 
     const onValid = async(data: I_Charactors) => {
         const API_URLs = `https://open.api.nexon.com/maplestory/v1/id?character_name=${data.charNm}`;
@@ -156,6 +210,20 @@ function Home(){
             await setCharID({charNm: data.charNm, ocid: Outputs.ocid});
             Navigate("/charToDo");
         }
+    };
+
+    const DeleteBookmark = (charNm: string) => {
+        const TargetIdx = SaveNms.findIndex((data) => data === charNm);
+
+        if(TargetIdx === -1){
+            return;
+        } else {
+            setSaveNms(() => {
+                const value = SaveNms.filter((data) => data !== charNm);
+                return value;
+            });
+            setEdits(false);
+        };
     };
 
     return (
@@ -181,6 +249,16 @@ function Home(){
             </Mains>
             <Bookmark_Box>
                 <B_Title>북마크</B_Title>
+                <EditBtn 
+                    isEdits={Edits} 
+                    isBookmark={SaveNms.length !== 0 ? true : false} 
+                    onClick={() => setEdits((prev) => !prev)}
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" height="13" width="13" viewBox="0 0 512 512">
+                        <path fill="rgb(225, 218, 218)" d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z"/>
+                    </svg>
+                    <span className="EditBtnText">편집</span>
+                </EditBtn>
                 <Bookmarks>
                     {
                         SaveNms.map((data) => {
@@ -189,8 +267,15 @@ function Home(){
                             };
 
                             return (
-                                <Bookmark_Item key={data} onClick={() => onValid(Converts)}>
-                                    {data}
+                                <Bookmark_Item isEdits={Edits} key={data}>
+                                    <div className="BookmarkText" onClick={() => onValid(Converts)}>
+                                        {data}
+                                    </div>
+                                    <DeleteBtn isEdits={Edits} onClick={() => DeleteBookmark(data)}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 384 512">
+                                            <path fill="rgb(250, 250, 250)" d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/>
+                                        </svg>
+                                    </DeleteBtn>
                                 </Bookmark_Item>
                             );
                         })
