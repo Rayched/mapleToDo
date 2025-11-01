2c9335908a92c2830b39a6c3ee9e86ebaa33d78f
Rayched <ny2401@naver.com>
2025-11-01 20:55:03 +0900

    20251101, api url 수정 (date 부분 삭제, 필수 아님)

--- src/modules/Fetchs.ts
+++ src/modules/Fetchs.ts
@@ -1,5 +1,4 @@
 import { I_Ocids } from "../Atoms";
-import { getDateTimes } from "./Dates";
 
 const Basic_URLs = "https://open.api.nexon.com/maplestory/v1";
 
@@ -10,9 +9,8 @@ export async function TestFetch(charID: string) {
 }
 
 export async function getCharData(charID: I_Ocids){
-    const DateTimes = getDateTimes();
 
-    const characterURLs = Basic_URLs + `/character/basic?ocid=${charID.ocid}&date=${DateTimes}`;
+    const characterURLs = Basic_URLs + `/character/basic?ocid=${charID.ocid}`;
 
     const CharacterData = await(await(
         await fetch(characterURLs, {
